be3b86f91978a5c09df1b527c307873fb5b304d1
Axelle Tortai <a.tortai14@gmail.com>
2018-11-22 23:33:01 +0100

    Show, Destroy, seeds

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -14,6 +14,9 @@ class EmailController < ApplicationController
     def destroy 
         @email = Email.find(params[:id])
         @email.destroy
+            respond_to do |format|
+                format.js   
+             end
     end 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "email#index"
 
   get 'email/show', to: "email#show"
+  get 'email/delete', to: "email#destroy"
 
   resources :email 
   
